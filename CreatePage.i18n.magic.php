18a95460f8a2a04c30a72140822892ee67105cdb
Markus Kroetzsch <markus@semantic-mediawiki.org>
2014-09-23 12:21:20 +0200

    Initial version of extension (experimental)

---
+++ CreatePage.i18n.magic.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Magic words
+ * 
+ * @file
+ */
+
+$magicWords = array();
+
+/** English (English) */
+$magicWords['en'] = array(
+	'createPage' => array( 0, 'createpageifnotex' ),
+);
